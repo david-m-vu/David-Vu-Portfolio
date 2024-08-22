a2144caaa3477dce9b0a1c81bbf93f640a8ecf51
David Vu <david.vu@berkeley.edu>
2024-08-22 15:58:55 -0700

    added placeholders for loading images

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,5 +1,5 @@
 import "./Project.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import externalLinkIcon from "../../assets/icons/external-link.png"
 
 const Project = (props) => {
@@ -15,7 +15,7 @@ const Project = (props) => {
                 </div>
                 <figure className="w-full">
                     <div className="thumbnail-container w-full relative" onMouseOver={() => setIsHoveringImage(true)} onMouseOut={() => setIsHoveringImage(false)}>
-                        <a href={props.link} target="_blank" rel="noopener noreferrer"><img className="rounded-lg thumbnail" src={props.thumbnail}></img></a>
+                        <a href={props.link} target="_blank" rel="noopener noreferrer"><ImageWithPlaceHolder alt={`${props.name} thumbnail`} src={props.thumbnail}></ImageWithPlaceHolder></a>
                         <img src={externalLinkIcon} className={`${!isHoveringImage && "icon-hidden"} external-link-icon h-[20px] w-[20px] absolute top-2 right-2 pointer-events-none	`}></img>
                     </div>
                     <figcaption className="caption mt-2 flex flex-col justify-between">
@@ -33,6 +33,36 @@ const Project = (props) => {
             </div>
         </div>
 
+    )
+}
+
+const ImageWithPlaceHolder = (props) => {
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [placeholderColor, setPlaceholderColor] = useState("#686b5e")
+
+    useEffect(() => {
+        setPlaceholderColor(getRandomDullHslColor());
+    }, [])
+
+    const getRandomDullHslColor = () => {
+        // Generate random hue (0-360)
+        const hue = Math.floor(Math.random() * 361);
+        // Set saturation to a low value to ensure dullness (e.g., 10-20%)
+        const saturation = Math.floor(Math.random() * 11) + 10;
+        // Set lightness to a low-medium value (e.g., 30-50%)
+        const lightness = Math.floor(Math.random() * 21) + 30;
+      
+        return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+    }
+
+    return (
+        <div className="flex flex-row justify-center">
+            <img className={`object-cover w-full h-full rounded-lg ${isLoaded ? "block" : "hidden"}`} src={props.src} alt={props.alt} style={{aspectRatio: 1.5}}
+                onLoad={() => {
+                    setIsLoaded(true); 
+                }}/>
+            {!isLoaded && <div className={`w-full h-full rounded-lg`} style={{ backgroundColor: placeholderColor, aspectRatio: 1.5 }}/>}
+        </div>
     )
 }
 
