82ea7ed7bca584afe2d8cb313278e3b2b69a8c0a
David Vu <david.vu@berkeley.edu>
2024-08-24 13:06:01 -0700

    made scroll to top on reroute

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter as Router } from "react-router-dom";
+import ScrollToTop from './components/ScrollToTop/ScrollToTop.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
+      <ScrollToTop/>
       <App />
     </Router>
   </StrictMode>,
